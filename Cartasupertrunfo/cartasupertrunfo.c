a3b763cd3b990a4c85cc5a64945950d8f6e00341
emanuel121314-sudo <emanuel121314@gmail.com>
2025-12-17 22:19:48 +0000

    COMMIT INTERMEDIARIO

--- Cartasupertrunfo/cartasupertrunfo.c
+++ Cartasupertrunfo/cartasupertrunfo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(){
+    int carta1, carta2;
     char Estado[100];
     char Cidade[100];
     char codigodacarta[5];
@@ -8,7 +9,16 @@ int main(){
     float area;
     float Pib;
     int pontosturisticos;
-
+    float densidadepopulacional;
+    float pibpercapita;
+   
+   
+    // primeira carta//
+    
+    printf("JOGO SUPER TRUNFO:\n");
+    
+    printf("Informações da primeira carta: \n");
+   
     printf("Digite o nome do estado: \n");
     scanf("%s" , Estado);
 
@@ -22,21 +32,64 @@ int main(){
     scanf("%d" , &população);
 
     printf("Digite a área: \n");
-    scanf("%2f" , &area);
+    scanf("%f" , &area);
 
     printf("Digite o PIB: \n");
-    scanf("1f" , &Pib);
+    scanf("%f" , &Pib);
 
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d" , &pontosturisticos);
+   
+    densidadepopulacional = população/area;
+    pibpercapita = Pib/população;
+    
     printf("Nome do estado: %s \n", Estado);
     printf("Nome da cidade: %s \n", Cidade);
     printf("Código da carta: %s \n", codigodacarta);
     printf("População: %d \n", população);
-    printf("Área: %2f \n", area);
-    printf("PIB: %2f \n", Pib);
+    printf("Área: %f \n", area);
+    printf("PIB: %f \n", Pib);
     printf("Pontos turísticos: %d \n", pontosturisticos);
+    printf("Densidade populacional: %2f\n", densidadepopulacional);
+    printf("PIB per capita: %2f\n" , pibpercapita);
+    
+    //segunda carta// 
 
+    printf("Digite as informações da segunda carta:\n");
+    
+    printf("Digite o nome do estado: \n");
+    scanf("%s" , Estado);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s" , Cidade);
+
+    printf("Digite o código da carta: \n");
+    scanf("%s" , codigodacarta);
+
+    printf("Digite a População: \n");
+    scanf("%d" , &população);
+
+    printf("Digite a área: \n");
+    scanf("%f" , &area);
+
+    printf("Digite o PIB: \n");
+    scanf("%f" , &Pib);
+
+    printf("Digite o número de pontos turísticos: \n");
+    scanf("%d" , &pontosturisticos);
+   
+    densidadepopulacional = população/area;
+    pibpercapita = Pib/população;
+    
+    printf("Nome do estado: %s \n", Estado);
+    printf("Nome da cidade: %s \n", Cidade);
+    printf("Código da carta: %s \n", codigodacarta);
+    printf("População: %d \n", população);
+    printf("Área: %f \n", area);
+    printf("PIB: %f \n", Pib);
+    printf("Pontos turísticos: %d \n", pontosturisticos);
+    printf("Densidade populacional: %2f\n", densidadepopulacional);
+    printf("PIB per capita: %2f\n" , pibpercapita);
     
      return 0;
 
---
+++ projetos/operadores.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){   
+   
+   float nota1, nota2, nota3;
+   float media;
+  
+
+   printf("Digite o primeiro valor:\n" );
+   scanf("%f" , &nota1);
+   printf("Digite o segundo valor: \n ");
+   scanf("%f" , &nota2);
+   printf("Digite o terceiro valor: \n" );
+   scanf("%f" , &nota3);
+   media = (nota1 + nota2 + nota3)/3 - (nota1 + nota2 - nota3);
+   printf("valor da media: %2f", media);
+   return 0;
+
+
+
+
+    
+}
